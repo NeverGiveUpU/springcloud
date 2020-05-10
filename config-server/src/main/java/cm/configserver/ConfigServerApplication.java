bf568d548bed0f7c70699e5b5e81e2934bf031de
cm <2786859763@qq.com>
2020-05-11 01:39:22 +0800

    服务配置demo

---
+++ config-server/src/main/java/cm/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package cm.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author 陈濛
+ * @date 2020/5/11 12:13 上午
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
--- webmvc/src/main/java/cm/webmvc/WebmvcApplication.java
+++ webmvc/src/main/java/cm/webmvc/WebmvcApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
 @EnableZuulProxy
 @EnableFeignClients
 @EnableCircuitBreaker
